86df0ac7e6d55e58026087e3fadde80a4430b56b
Utkarsh <utkarshkumark@gmail.com>
2019-11-03 18:42:06 +0530

    added comments to scraper for readability

--- scraper.py
+++ scraper.py
@@ -11,15 +11,18 @@ from urllib.request import Request, urlopen
 
 from getUrl import url
 
+# For ignoring SSL certificate errors
 ct = ssl.create_default_context()
 ct.check_hostname = False
 ct.verify_mode = ssl.CERT_NONE
 
+# Making the website believe that you are accessing it using a browser
 req = Request(url, headers = {'User-Agent' : 'Chrome/77.0.3865.120' })
 webpage = urlopen(req).read()
 
 # print(webpage)
 
+# Creating a BeautifulSoup object of the html page for extraction of data
 soup = BeautifulSoup(webpage, 'html.parser')
 html = soup.prettify('utf-8')
 song_json = {}
@@ -27,12 +30,16 @@ song_json["Lyrics"] = []
 song_json["Comments"] = []
 # print(html)
 
+#Extract Title of the song
 for title in soup.findAll('title'):
     song_json["Title"] = title.text.strip()
 
+# Extract the release date of the song
 for span in soup.findAll('span', attrs = {'class' : 'metadata_unit-info metadata_unit-info--text_only'}):
     song_json["Release Date"] = span.text.strip()
 
+
+#Extract the Lyrics of the song
 for div in soup.findAll('div', attrs = {'class': 'lyrics'}):
   song_json["Lyrics"].append(div.text.strip().split("\n"))
 
@@ -43,7 +50,10 @@ for div in soup.findAll('div', attrs = {'class': 'rich_text_formatting'}):
       if comment!="":
           song_json["Comments"].append(comment)
 
+#Save the json created with the file name as title + .json
 with open(song_json["Title"] + '.json', 'w') as outfile:
   json.dump(song_json, outfile, indent = 4, ensure_ascii = False)
 
-print(song_json)
+# print(song_json)
+
+print('----------Extraction of data is complete. Check json file.----------')
