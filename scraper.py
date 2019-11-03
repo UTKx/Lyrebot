397ef71aef3d474fc7f279b4f8b71c45879a01a7
Utkarsh <utkarshkumark@gmail.com>
2019-11-03 18:30:12 +0530

    scraped title, date and title working fine till now

--- scraper.py
+++ scraper.py
@@ -18,4 +18,21 @@ ct.verify_mode = ssl.CERT_NONE
 req = Request(url, headers = {'User-Agent' : 'Chrome/77.0.3865.120' })
 webpage = urlopen(req).read()
 
-print(webpage)
+# print(webpage)
+
+soup = BeautifulSoup(webpage, 'html.parser')
+html = soup.prettify('utf-8')
+song_json = {}
+song_json["Lyrics"] = []
+# song_json["Comments"] = []
+# print(html)
+
+for title in soup.findAll('title'):
+    song_json["Title"] = title.text.strip()
+
+for span in soup.findAll('span', attrs = {'class' : 'metadata_unit-info metadata_unit-info--text_only'}):
+    song_json["Release Date"] = span.text.strip()
+
+for div in soup.findAll('div', attrs = {'class': 'lyrics'}):
+  song_json["Lyrics"].append(div.text.strip().split("\n"))
+
