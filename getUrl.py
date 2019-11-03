f0823d2f018f0e49d25c3515c0419e70e30ad472
Utkarsh <utkarshkumark@gmail.com>
2019-11-03 18:55:03 +0530

    added comments to getUrl.py

--- getUrl.py
+++ getUrl.py
@@ -1,18 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Enter the desired Song Name
 song_name = input("Enter the song name (Eg. Artist name song name): ")
 lyrics = "-lyrics"
 
+# Conversion to URL
 raw_song_name = song_name.replace(" ", "-")
 
+#  URL for the desired song
 url_song_name = raw_song_name + lyrics
 
+# Final URL
 url = f"https://genius.com/{url_song_name}"
 
+# Extraction of the data
 res = requests.get(url)
 src = res.content
 
+# Parsing data using html parser
 soup = BeautifulSoup(src, "html.parser")
 
-print(soup.prettify)
+# print(soup.prettify)
